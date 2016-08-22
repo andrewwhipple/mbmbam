efd15a2694b6513d8e9890d244d83864700be2b3
Andrew Whipple <anwhipple@C1MRQLVFH3QF.group.on>
2016-08-22 15:40:53 -0500

    Some initial mongodb setup and schema definitions

--- index.js
+++ index.js
@@ -2,6 +2,36 @@ var express = require('express');
 var app = express();
 var hbs = require('express-handlebars');
 
+var mongoose = require('mongoose');
+
+
+//mongoose.connect('mongodb://localhost/mbmbam_database');
+
+var Schema = mongoose.Schema, 
+    ObjectId = Scheme.ObjectId;
+
+var Episode = new Schema({
+    title: String,
+    epNum: Number,
+    description: String,
+    mp3: String,
+    pubDate: Date,
+    brothers: [Brother]
+});
+
+var Clip = new Schema({
+    episode: Episode,
+    inPoint: Number,
+    outPoint: Number,
+    mvp: Brother
+});
+
+var Brother = new Schema({
+    name: String,
+    description: String
+});
+
+
 app.engine('handlebars', hbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
